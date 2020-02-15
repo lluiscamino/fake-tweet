9d8fe78937cbfe0c6166b151303cb99e47261158
Lluís Camino <caminoperez.lluis@gmail.com>
2020-02-15 15:43:00 +0100

    Updates

---
+++ precache-manifest.a722551f2b3bb2847ae8b30ebf9a73ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1afdb4ae90628749c22ca63da7c3413",
+    "url": "/fake-tweet/index.html"
+  },
+  {
+    "revision": "8d91698e44d08bd0e6b0",
+    "url": "/fake-tweet/static/css/main.ad6d0711.chunk.css"
+  },
+  {
+    "revision": "92c95cc577d0cb2a9935",
+    "url": "/fake-tweet/static/js/2.8db82a92.chunk.js"
+  },
+  {
+    "revision": "60b1fe6729a692d59d23f02b8b1ed8a3",
+    "url": "/fake-tweet/static/js/2.8db82a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d91698e44d08bd0e6b0",
+    "url": "/fake-tweet/static/js/main.096a560e.chunk.js"
+  },
+  {
+    "revision": "701626bdd9b85f8a08f7",
+    "url": "/fake-tweet/static/js/runtime-main.e5c4631b.js"
+  }
+]);
